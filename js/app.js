34a139de4e1ca66b98533f5b5553c995033a4035
Prakash <prakad@amazon.com>
2014-12-20 22:19:54 +0530

    First Commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+/**
+ * This file is provided by Facebook for testing and evaluation purposes
+ * only. Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// This file bootstraps the entire application.
+
+var ChatApp = require('./components/ChatApp.react');
+var ChatExampleData = require('./ChatExampleData');
+var ChatWebAPIUtils = require('./utils/ChatWebAPIUtils');
+var React = require('react');
+var injectTapEventPlugin = require("react-tap-event-plugin"); // Material-UI dependency
+
+window.React = React; // export for http://fb.me/react-devtools
+
+ChatExampleData.init(); // load example data into localstorage
+
+ChatWebAPIUtils.getAllMessages();
+
+//Material-UI dependency
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
+React.render(
+    <ChatApp />,
+    document.getElementById('react')
+);
