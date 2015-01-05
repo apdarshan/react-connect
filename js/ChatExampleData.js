a0849e3a6353eb4184a1e24a013bd09943121cb9
apdarshan@gmail.com <apdarshan@gmail.com>
2015-01-05 13:07:06 +0530

    Example data reduced

--- js/ChatExampleData.js
+++ js/ChatExampleData.js
@@ -23,7 +23,7 @@ module.exports = {
         id: 'm_1',
         threadID: 't_1',
         threadName: 'Jing and '+ yourLabel,
-        authorName: currentUserName,
+        authorName: 'Me',
         text: 'Hey Jing, want to give a Flux talk at ForwardJS?',
         timestamp: Date.now() - 99999
       },
@@ -31,7 +31,7 @@ module.exports = {
         id: 'm_2',
         threadID: 't_1',
         threadName: 'Jing and ' + yourLabel,
-        authorName: currentUserName,
+        authorName: 'Me',
         text: 'Seems like a pretty cool conference.',
         timestamp: Date.now() - 89999
       },
@@ -42,38 +42,6 @@ module.exports = {
         authorName: 'Jing',
         text: 'Sounds good.  Will they be serving dessert?',
         timestamp: Date.now() - 79999
-      },
-      {
-        id: 'm_4',
-        threadID: 't_2',
-        threadName: 'Dave and '+ yourLabel,
-        authorName: currentUserName,
-        text: 'Hey Dave, want to get a beer after the conference?',
-        timestamp: Date.now() - 69999
-      },
-      {
-        id: 'm_5',
-        threadID: 't_2',
-        threadName: 'Dave and '+ yourLabel,
-        authorName: 'Dave',
-        text: 'Totally!  Meet you at the hotel bar.',
-        timestamp: Date.now() - 59999
-      },
-      {
-        id: 'm_6',
-        threadID: 't_3',
-        threadName: yourLabel + ' and Brian',
-        authorName: currentUserName,
-        text: 'Hey Brian, are you going to be talking about functional stuff?',
-        timestamp: Date.now() - 49999
-      },
-      {
-        id: 'm_7',
-        threadID: 't_3',
-        threadName: yourLabel + ' and Brian',
-        authorName: 'Brian',
-        text: 'At ForwardJS?  Yeah, of course.  See you there!',
-        timestamp: Date.now() - 39999
       }];
   },
 
